da12f1a6343a744263a53707a84c3740dcc46e27
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-05-27 10:45:18 -0500

    second commit

---
+++ src/data/index.js
@@ -0,0 +1,22 @@
+const initialRequests = [
+    {
+        id: '100',
+        requester: 'Rajesh R',
+        project: 'my project',
+        cluster: 'cluster 0',
+        database: 'mydb',
+        role: 'admin',
+        status: 'OPEN'
+    },
+    {
+        id: '200',
+        requester: 'Rajesh R',
+        project: 'my project',
+        cluster: 'cluster 0',
+        database: 'mydb',
+        role: 'user',
+        status: 'OPEN'
+    }
+]
+
+module.exports = { initialRequests }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import 'todomvc-app-css/index.css'
+
+import App from './components/App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
